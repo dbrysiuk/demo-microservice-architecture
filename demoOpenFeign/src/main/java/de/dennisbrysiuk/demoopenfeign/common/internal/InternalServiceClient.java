6c4121809bb3c1987e7b82c43249683aa7a841f8
dennis.brysiuk <dennis.brysiuk@accenture.com>
2022-12-09 15:39:55 +0100

    Demo for Microservice Architecture Part 1 - remove typo

--- demoOpenFeign/src/main/java/de/dennisbrysiuk/demoopenfeign/common/internal/InternalServiceClient.java
+++ demoOpenFeign/src/main/java/de/dennisbrysiuk/demoopenfeign/common/internal/InternalServiceClient.java
@@ -3,7 +3,7 @@ package de.dennisbrysiuk.demoopenfeign.common.internal;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "InternalServiceClinet", url = "http://localhost:7080")
+@FeignClient(name = "InternalServiceClient", url = "http://localhost:7080")
 public interface InternalServiceClient {
 
     @GetMapping("/")
